328f579fd4f7aadbbf7f179319f2cc7279e19e0f
Devops <salmantariq8018@gmail.com>
2026-06-27 15:12:57 +0500

    chore: initial project scaffold with Vite client and Express server

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes 
+
+export default app;
