013addb3ff18523eb019da8f4ee8daefb37f6eba
Devops <salmantariq8018@gmail.com>
2026-06-28 16:20:37 +0500

    feat(db): setup MongoDB connection with mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+};
+
+startServer();
